bc030143b8ba7ee39dc07de353dfc0513be7b0c3
sennninsyou <fujimon1411@gmail.com>
2023-10-23 00:44:04 +0900

    commit

--- send.php
+++ send.php
@@ -4,7 +4,7 @@ use Minishlink\WebPush\WebPush;
 
 $auth = [
     'VAPID' => [
-        'subject' => 'mailto:me@website.com', // can be a mailto: or your website address
+        'subject' => 'https://sennninsyou.github.io/test/', // can be a mailto: or your website address
         'publicKey' => 'BC1VHFZkXPFeQxUZcPFaKmB0ybdEoJDP0NtRpRQcm9r1wDs59EP6HRxkBPmTqM5-I7YqPvuXh5WA2qVVozLsw4k', // (recommended) uncompressed public key P-256 encoded in Base64-URL
         'privateKey' => 'StpkspHJM47XrXgAIcJbmUqlVtdWsVfLTdKo9BEEMq8', // (recommended) in fact the secret multiplier of the private key encoded in Base64-URL
     ],
