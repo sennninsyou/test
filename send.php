14bd31cc10a5296f9e7fc15c8f662815c2085e2e
sennninsyou <fujimon1411@gmail.com>
2023-10-23 13:14:36 +0900

    commit

--- send.php
+++ send.php
@@ -15,6 +15,6 @@ $webPush = new WebPush($auth);
 
 $report = $webPush->sendOneNotification(
     Subscription::create(json_decode('{"endpoint":"https://fcm.googleapis.com/fcm/send/cXtN_ByxeHc:APA91bHwOaLbx-QJWOXUuRU2AD9jAjQHVWTHL5THVGClVxAwuEjcTlVIYaALiq7svInQaxHjjZnyfvPhCL5R__i_mU0aRBeaU3VSYyhRi6g6kDUraL-ByVIPzCusEeRxGmi04rKPG2b8","expirationTime":null,"keys":{"p256dh":"BEYYk1SOfksoOMBFbPUR_6LWx8v1DzqTm0lerUjbmG-ahvUDD9PIf3_VyQVdyHxr7HrHAHoYX4R0km-Y-mbrEwo","auth":"lyFJ-CsKRQuf5hZEulmvzw"}}',true))
-    , '{"title":"Hi", "body":"a", "url":"./"}', ['TTL' => 5000]);
+    , '{"title":"通知テスト", "body":"これはPush通知のテストです。", "url":"./"}', ['TTL' => 5000]);
 
     print_r($report);
